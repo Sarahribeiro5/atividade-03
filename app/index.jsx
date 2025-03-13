9c00d8f5aaf77db23b137ea819deafe5acc86c84
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2025-03-13 14:49:32 -0300

    feat: add image on index

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 export default function Home() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f0f8ff', padding: 30 }}>
-            <Text style={{ color: '#4b0082', fontSize: 24, marginBottom: 15, fontWeight: "bold", textAlign: 'center' }}>Bem-vindo ao Monster High Hub</Text>
+            <Text style={{ color: '#4b0082', fontSize: 24, marginBottom: 15, fontWeight: "bold", textAlign: 'center' }}>
+                Bem-vindo ao Monster High Hub
+            </Text>
             <Text style={{ color: '#333', fontSize: 18, textAlign: 'center', lineHeight: 24 }}>
                 Venha conhecer o universo de Monster High. Um desenho animado cheio de personagens estilosas, fofas e cativantes.
             </Text>
+            <Image
+                source={{ uri: 'https://files.ekmcdn.com/allwallpapers/images/monster-high-paper-photo-wallpaper-33396-p.jpg' }}
+                style={{ width: 300, height: 200, marginTop: 15 }}
+                resizeMode="contain" 
+            />
         </View>
     );
 }
